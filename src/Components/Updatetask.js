c06a4a0102771339daae4cb838a62dcda8110439
swathiravi-vh <swathiit01@gmail.com>
2023-03-22 12:56:08 +0530

    todolistapp

---
+++ src/Components/Updatetask.js
@@ -0,0 +1,31 @@
+import './Updatetask.css'
+import React ,{useState} from 'react'
+import axios from 'axios'
+
+function Updatetask(props) {
+    const [task,setTask] = useState(props.task.todo)
+    const updateTask = () => {
+        if(task.trim() === '' || props.task.todo === task){
+            props.removePopup()
+        } else {
+            axios.put(`http://localhost:3001/api/tasks/${props.task._id}`,{
+                _id : props.task._id,
+                todo : task,
+                isComplete : props.task.isComplete
+            }).then(res => {
+                props.removePopup()
+                props.updatetask(res.data)
+            }).catch(err => console.log(err))
+        }
+    }
+    return (
+        <div className = 'popup'>
+            <div className = 'popup-content'>
+                <input type = 'text'  placeholder = 'Update Task . . .' value = {task} onChange = {event => setTask(event.target.value)}/>
+                <button onClick = {() => updateTask()}>Update</button>
+            </div>
+        </div>
+    )
+}
+
+export default Updatetask
